4498898f3f3d86c1523cb79fb16da4b699f6a18c
Mary-Kate James <marykatj@gmail.com>
2018-04-12 18:48:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
    var dwarfString = "";
-  for (let i = 0; i > (dwarves.length/2); i++) {
+  for (let i = (dwarves.length/2); i < dwarves.length; i++) {
     dwarfString += (i+1).toString() + ". " + dwarves[i] + " ";
      }
      return dwarfString;
