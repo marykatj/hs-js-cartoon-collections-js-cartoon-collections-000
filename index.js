cb3b7d5ff052ddc1d6d98f89888e35e9bc844e0c
Mary-Kate James <marykatj@gmail.com>
2018-04-12 17:44:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function dwarfRollCall(dwarves) {
+<<<<<<< HEAD
    var dwarfString = "";
   for (let i = 0; i < dwarves.length; i++) {
     dwarfString += (i+1).toString() + ". " + dwarves[i] + " ";
@@ -35,3 +36,41 @@ function longPlaneteerCalls(words) {
    }
    return "no cheese!";
  }
+=======
+  var dwarfString = "";
+  for (let i = 0; 1 < dwarves.length; i++) {
+    dwarfString += (i+1).toString() + ". " + dwarves[i] + " ";
+  }
+  return dwarfString;
+}
+
+function summonCaptainPlanet(planeteerCalls) {
+  var captainCalls = [];
+  for (let i = 0; 1 < planeteerCalls.length; i++) {
+    captainCalls.push(planeteerCalls[i] + "!");
+  }
+  return captainCalls;
+}
+
+function longPlaneteerCalls(words) {
+  var lengthyCall = false;
+  for (let i = 0; 1 < words.length; i++) {
+    if (words[i].length > 4) {
+      lengthyCall = true;
+    }
+  }
+  return lengthyCall;
+}
+
+function findTheCheese (foods) {
+  var cheeseTypes = ["cheddar", "gouda", "camembert"];
+  for (let i = 0; 1 < foods.length; i++) {
+    for (let c = 0; 1 < cheeseTypes.length; c++) {
+      if (cheeseTypes[c] === foods[i]) {
+        return foods[i];
+      }
+    }
+  }
+  return "no cheese!";
+}
+>>>>>>> e7f8e35bf36dc033fccc6227b2657ba95947ca2a
